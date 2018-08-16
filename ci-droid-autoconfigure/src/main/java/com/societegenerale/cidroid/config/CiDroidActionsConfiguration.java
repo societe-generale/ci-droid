4f71837f186e047b26ccaf9790ac4ea18ba54d9a
vfuchs070114 <vincent.fuchs@sgcib.com>
2018-08-16 11:34:14 +0530

    upgrading ci-droid-extensions.version to 1.0.4 and instantiating the new available actions

--- ci-droid-autoconfigure/src/main/java/com/societegenerale/cidroid/config/CiDroidActionsConfiguration.java
+++ ci-droid-autoconfigure/src/main/java/com/societegenerale/cidroid/config/CiDroidActionsConfiguration.java
@@ -1,9 +1,7 @@
 package com.societegenerale.cidroid.config;
 
-import com.societegenerale.cidroid.extensions.actionToReplicate.OverwriteStaticFileAction;
-import com.societegenerale.cidroid.extensions.actionToReplicate.ReplaceMavenProfileAction;
-import com.societegenerale.cidroid.extensions.actionToReplicate.SimpleReplaceAction;
-import com.societegenerale.cidroid.extensions.actionToReplicate.TemplateBasedContentAction;
+import com.societegenerale.cidroid.api.actionToReplicate.ActionToReplicate;
+import com.societegenerale.cidroid.extensions.actionToReplicate.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,27 +9,45 @@ import org.springframework.context.annotation.Configuration;
 public class CiDroidActionsConfiguration {
 
     @Bean
-    public OverwriteStaticFileAction overwriteStaticFileAction() {
+    public ActionToReplicate overwriteStaticFileAction() {
 
         return new OverwriteStaticFileAction();
     }
 
     @Bean
-    public ReplaceMavenProfileAction replaceMavenProfileAction() {
+    public ActionToReplicate replaceMavenProfileAction() {
 
         return new ReplaceMavenProfileAction();
     }
 
     @Bean
-    public SimpleReplaceAction simpleReplaceAction() {
+    public ActionToReplicate simpleReplaceAction() {
 
         return new SimpleReplaceAction();
     }
 
     @Bean
-    public TemplateBasedContentAction templateBasedContentAction() {
+    public ActionToReplicate addXmlElementAction() {
+
+        return new AddXmlElementAction();
+    }
+
+    @Bean
+    public ActionToReplicate removeXmlElementAction() {
+
+        return new RemoveXmlElementAction();
+    }
+
+    @Bean
+    public ActionToReplicate templateBasedContentAction() {
 
         return new TemplateBasedContentAction();
+    }
+
+    @Bean
+    public ActionToReplicate removeMavenDependencyOrPluginAction() {
+
+        return new RemoveMavenDependencyOrPluginAction();
     }
 
 }
