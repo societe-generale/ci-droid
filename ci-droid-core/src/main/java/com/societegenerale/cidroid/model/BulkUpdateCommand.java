866870d1d58dcf30143f7783ae52c4b517e4411a
vfuchs070114 <vincent.fuchs@sgcib.com>
2018-08-17 08:34:14 +0530

    added Swagger docs

--- ci-droid-core/src/main/java/com/societegenerale/cidroid/model/BulkUpdateCommand.java
+++ ci-droid-core/src/main/java/com/societegenerale/cidroid/model/BulkUpdateCommand.java
@@ -3,11 +3,12 @@ package com.societegenerale.cidroid.model;
 import com.societegenerale.cidroid.api.ResourceToUpdate;
 import com.societegenerale.cidroid.api.actionToReplicate.ActionToReplicate;
 import com.societegenerale.cidroid.api.gitHubInteractions.AbstractGitHubInteraction;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
@@ -18,24 +19,31 @@ import java.util.List;
 @ToString(exclude = "gitHubOauthToken")
 public class BulkUpdateCommand {
 
+    @ApiModelProperty(position = 1, value="the git login to use when we'll commit the change", required = true)
     @NotEmpty
     private String gitLogin;
 
+    @ApiModelProperty(position = 2, required = true)
     @NotEmpty
     private String gitHubOauthToken;
 
+    @ApiModelProperty(position = 3, required = true)
     @Email
     private String email;
 
+    @ApiModelProperty(position = 4, required = true)
     @NotEmpty
     private String commitMessage;
 
+    @ApiModelProperty(position = 5, required = true)
     @NotNull
     private ActionToReplicate updateAction;
 
+    @ApiModelProperty(position = 6, required = true)
     @Valid
     private AbstractGitHubInteraction gitHubInteractionType;
 
+    @ApiModelProperty(position = 7, required = true)
     @NotEmpty
     private List<ResourceToUpdate> resourcesToUpdate;
 
