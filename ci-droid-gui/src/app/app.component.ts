bfe2f04d68e1a91ee8a1e0ae0a5a647434eb159a
paul58914080 <paul58914080@gmail.com>
2018-12-21 12:31:06 +0530

    Initial commit for gui

---
+++ ci-droid-gui/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ci-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+}
