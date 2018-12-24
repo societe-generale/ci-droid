2b84fb7338c717a6d7571ce2af87d6fee4cf2652
paul58914080 <paul58914080@gmail.com>
2018-12-24 08:38:31 +0530

    Fixed the formatting

--- ci-droid-gui/src/app/app.module.ts
+++ ci-droid-gui/src/app/app.module.ts
@@ -1,21 +1,14 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {LoggerModule, NgxLoggerLevel} from 'ngx-logger';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    LoggerModule.forRoot({ level: NgxLoggerLevel.INFO })
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, LoggerModule.forRoot({ level: NgxLoggerLevel.INFO })],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
