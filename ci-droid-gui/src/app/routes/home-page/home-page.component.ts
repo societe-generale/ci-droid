b1ddcdf81ae220bf3d2fe127fe950b3e6fcbe6a9
J-DK <dileepkumarjami@gmail.com>
2019-01-04 11:40:53 +0530

    Addressed review comments

--- ci-droid-gui/src/app/routes/home-page/home-page.component.ts
+++ ci-droid-gui/src/app/routes/home-page/home-page.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ci-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent {
   constructor() {}
-
-  ngOnInit() {}
 }
