1149f1cdad3e7d099f072329a28e18a6b4ac6458
J-DK <dileepkumarjami@gmail.com>
2019-01-04 12:07:21 +0530

    Feature/home page (#14)
    * Modularised Routes
    
    * Added home page component
    
    * Addressed review comments

---
+++ ci-droid-gui/src/app/routes/home-page/home-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ci-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent {
+  constructor() {}
+}
