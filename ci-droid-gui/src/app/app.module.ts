7a472cefb7253671c77e2e9abc4a30767c44b588
Paul Williams <paul58914080@gmail.com>
2019-01-07 10:09:36 +0530

    Feature add material design to the gui (#15)
    * Upgraded the dependency versions to fix node-sass issue and added material
    
    * defined the color palette for material
    
    * Included material button in the homepage for get started

--- ci-droid-gui/src/app/app.module.ts
+++ ci-droid-gui/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppComponent } from './app.component';
 import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 import { RoutesModule } from './routes/routes.module';
 import { HomePageModule } from './routes/home-page/home-page.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RoutesModule, HomePageModule, LoggerModule.forRoot({ level: NgxLoggerLevel.INFO })],
+  imports: [BrowserModule, RoutesModule, HomePageModule, LoggerModule.forRoot({ level: NgxLoggerLevel.INFO }), BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- ci-droid-gui/src/app/routes/home-page/home-page.module.ts
+++ ci-droid-gui/src/app/routes/home-page/home-page.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HomePageComponent } from './home-page.component';
+import { MatButtonModule } from '@angular/material';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   declarations: [HomePageComponent]
 })
 export class HomePageModule {}
