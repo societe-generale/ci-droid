d13170250e81fd71a11f7417798062674f30b484
J-DK <dileepkumarjami@gmail.com>
2019-01-14 15:30:55 +0530

    Made the routing from home-page to form

--- ci-droid-gui/src/app/routes/home-page/home-page.component.ts
+++ ci-droid-gui/src/app/routes/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ci-home-page',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
-  constructor() {}
+  constructor(private router: Router) {}
+
+  navigateToForm() {
+    this.router.navigate(['/form']);
+  }
 }
--- ci-droid-gui/src/app/routes/routes.ts
+++ ci-droid-gui/src/app/routes/routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { FormComponent } from './form/form.component';
 
-export const appRoutes: Routes = [{ path: '', component: HomePageComponent }];
+export const appRoutes: Routes = [{ path: '', component: HomePageComponent }, { path: 'form', component: FormComponent }];
