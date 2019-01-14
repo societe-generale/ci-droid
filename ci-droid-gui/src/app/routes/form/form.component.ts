c52aa50ff24f0a15874191bbb5eae045094dc235
J-DK <dileepkumarjami@gmail.com>
2019-01-14 15:26:21 +0530

    Created form component

---
+++ ci-droid-gui/src/app/routes/form/form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ci-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements OnInit {
+  constructor() {}
+
+  public ngOnInit() {}
+}
