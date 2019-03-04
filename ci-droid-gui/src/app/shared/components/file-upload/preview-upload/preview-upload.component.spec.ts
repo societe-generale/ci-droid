755033b4fc23d4ff6bc401f80b29f72b14675232
J-DK <dileepkumarjami@gmail.com>
2019-03-04 12:44:56 +0530

    refactor(upload-csv): Modularized the upload-csv to file-upload module

--- ci-droid-gui/src/app/shared/components/file-upload/preview-upload/preview-upload.component.spec.ts
+++ ci-droid-gui/src/app/shared/components/file-upload/preview-upload/preview-upload.component.spec.ts
@@ -1,8 +1,6 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCheckboxModule, MatTableModule } from '@angular/material';
-import { By } from '@angular/platform-browser';
-
 import { PreviewUploadComponent } from './preview-upload.component';
 
 describe('PreviewUploadComponent', () => {
