c46f52783c3e71912859ff2bafd7486d2b1e5745
J-DK <dileepkumarjami@gmail.com>
2019-03-06 14:20:04 +0530

    refactor(status-component): Enumerated the status states

--- ci-droid-gui/src/app/shared/modules/status/status.component.spec.ts
+++ ci-droid-gui/src/app/shared/modules/status/status.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { StatusComponent } from './status.component';
+import { StatusState } from './status.enum';
 
 describe('StatusComponent', () => {
   let component: StatusComponent;
@@ -10,9 +11,8 @@ describe('StatusComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [BrowserAnimationsModule],
-      declarations: [ StatusComponent ]
-    })
-    .compileComponents();
+      declarations: [StatusComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -26,12 +26,12 @@ describe('StatusComponent', () => {
   });
 
   it('should change the state when the animation is triggered', () => {
-     component.triggerAnimation();
-     expect(component.state).toBe('initial');
+    component.triggerAnimation();
+    expect(component.state).toBe(StatusState.INITIAL);
   });
 
   it('should change the state when the animation is reset', () => {
     component.reset();
-    expect(component.state).toBe('final');
+    expect(component.state).toBe(StatusState.FINAL);
   });
 });
--- ci-droid-gui/src/app/shared/modules/status/status.component.ts
+++ ci-droid-gui/src/app/shared/modules/status/status.component.ts
@@ -1,10 +1,8 @@
 import { Component, Input, OnInit, NgZone } from '@angular/core';
 import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
+import { StatusState } from './status.enum';
 
-const keyFrames = [
-  style({strokeDashoffset: 1000, offset: 0}),
-  style({strokeDashoffset: 0, offset: 1})
-];
+const keyFrames = [style({ strokeDashoffset: 1000, offset: 0 }), style({ strokeDashoffset: 0, offset: 1 })];
 
 @Component({
   selector: 'ci-status',
@@ -12,53 +10,60 @@ const keyFrames = [
   styleUrls: ['./status.component.scss'],
   animations: [
     trigger('tick', [
-      state('initial', style({
-        strokeDasharray: 1000,
-        strokeDashoffset: -100
-      })),
-      transition('initial => final', animate('2.5s .35s ease-in-out',
-        keyframes([
-          style({strokeDashoffset: -100, offset: 0}),
-          style({strokeDashoffset: 900, offset: 1})
-        ])))
+      state(
+        StatusState.INITIAL,
+        style({
+          strokeDasharray: 1000,
+          strokeDashoffset: -100
+        })
+      ),
+      transition(
+        `${StatusState.INITIAL} => ${StatusState.FINAL}`,
+        animate(
+          '2.5s .35s ease-in-out',
+          keyframes([style({ strokeDashoffset: -100, offset: 0 }), style({ strokeDashoffset: 900, offset: 1 })])
+        )
+      )
     ]),
     trigger('line', [
-      state('initial', style({
-        strokeDasharray: 1000,
-        strokeDashoffset: 1000
-      })),
-      transition('initial => final', animate('2.5s .35s ease-in-out',
-        keyframes(keyFrames)))
+      state(
+        StatusState.INITIAL,
+        style({
+          strokeDasharray: 1000,
+          strokeDashoffset: 1000
+        })
+      ),
+      transition(`${StatusState.INITIAL} => ${StatusState.FINAL}`, animate('2.5s .35s ease-in-out', keyframes(keyFrames)))
     ]),
     trigger('circle', [
-      state('initial', style({
-        strokeDasharray: 1000,
-        strokeDashoffset: 0
-      })),
-      transition('initial => final', animate('0.9s ease-in-out',
-        keyframes(keyFrames)))
+      state(
+        StatusState.INITIAL,
+        style({
+          strokeDasharray: 1000,
+          strokeDashoffset: 0
+        })
+      ),
+      transition(`${StatusState.INITIAL} => ${StatusState.FINAL}`, animate('0.9s ease-in-out', keyframes(keyFrames)))
     ])
   ]
 })
 export class StatusComponent implements OnInit {
-
   @Input() success;
   state: string;
 
-  constructor(public zone: NgZone) {
-  }
+  constructor(public zone: NgZone) {}
 
   ngOnInit() {
     this.triggerAnimation();
   }
 
   triggerAnimation() {
-    this.state = 'initial';
+    this.state = StatusState.INITIAL;
   }
 
   reset() {
     this.zone.run(() => {
-      this.state = 'final';
+      this.state = StatusState.FINAL;
     });
   }
 }
