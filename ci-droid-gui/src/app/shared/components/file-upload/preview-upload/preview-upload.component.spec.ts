40ef4851d9f1b2a7178d8f3e7d3d0a28d2e9a299
J-DK <dileepkumarjami@gmail.com>
2019-03-12 17:32:59 +0530

    fix(preview-upload): Preserved the selected resources when the preview is toggled

--- ci-droid-gui/src/app/shared/components/file-upload/preview-upload/preview-upload.component.spec.ts
+++ ci-droid-gui/src/app/shared/components/file-upload/preview-upload/preview-upload.component.spec.ts
@@ -1,11 +1,21 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { SelectionModel } from '@angular/cdk/collections';
 import { MatCheckboxModule, MatTableModule } from '@angular/material';
 import { PreviewUploadComponent } from './preview-upload.component';
+
+import ResourcesToUpdate = shared.types.ResourcesToUpdate;
 
 describe('PreviewUploadComponent', () => {
   let component: PreviewUploadComponent;
   let fixture: ComponentFixture<PreviewUploadComponent>;
+  const resource = [
+    {
+      repoFullName: 'societe-generale/ci-droid',
+      filePathOnRepo: 'Jenkinsfile',
+      branchName: 'master'
+    }
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -24,16 +34,26 @@ describe('PreviewUploadComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  it('should select all resources', () => {
+
+  it('should select all checked resources', () => {
     expect(component).toBeTruthy();
-    component.dataSource.data = [
-      {
-        repoFullName: 'societe-generale/ci-droid',
-        filePathOnRepo: 'Jenkinsfile',
-        branchName: 'master'
-      }
-    ];
-    component.selectAll();
+    component.selectedRows = resource;
+    component.selectCheckedResources();
     expect(component.selectedResources.selected.length).toEqual(1);
+  });
+
+  describe('should test the selection', () => {
+    it('should clear the selection when all are resources are selected', () => {
+      component.dataSource.data = resource;
+      component.selectedResources = new SelectionModel<ResourcesToUpdate>(true, resource);
+      component.selectAll();
+      expect(component.selectedResources.selected.length).toBe(0);
+    });
+
+    it('should select all resources when none is selected', () => {
+      component.dataSource.data = resource;
+      component.selectAll();
+      expect(component.selectedResources.selected.length).toBe(1);
+    });
   });
 });
